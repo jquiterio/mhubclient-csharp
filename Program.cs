453ff5212681ced1f0c7ca05c17e441822310623
AmandaAbarte <91249265+AmandaAbarte@users.noreply.github.com>
2023-08-07 16:15:15 +0200

    classes - Message & HubClient

--- Program.cs
+++ Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// namespace for the client
+namespace MHubClient
+{
+    public class Message
+    {
+        // properties
+        public string SubscriberID { get; set; }
+        public string Topic { get; set; }
+        public string Payload { get; set; }
+
+        // func NewMessage
+        public Message(string SubscriberID, string Topic, string Payload)
+        {
+            SubscriberID = subscriberID;
+            Topic = topic;
+            Payload = payload;
+        }
+        public static Message NewMessage(string subscriberID, string topic, string payload)
+        {
+            return new Message(subscriberID, topic, payload);
+        }
+        //!todo tostring
+    }
+    public class HubClient
+    {
+        // properties
+        public string subscriberID { get; set; }
+        public string Topics { get; set; }
+        public Action<Message> handler { get; set; } // func(Message)
+        public IPEndPoint Address { get; set; } // net.TCPAddr
+        public SslStream Conn { get; set; } // tls.Conn
+        public bool Debug { get; set; }
+
+    }
+    class Programs
+    {
+        // main func
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
