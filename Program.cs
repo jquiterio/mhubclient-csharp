0c6cd557d43e6ffa95b5efea64a555a3f26855e6
AmandaAbarte <91249265+AmandaAbarte@users.noreply.github.com>
2023-08-09 10:09:21 +0200

    init the client

--- Program.cs
+++ Program.cs
@@ -1,35 +1,50 @@
 ﻿// namespace for the client
 namespace MHubClient
 {
+    using System;
+    using System.Net;
     public class Message
     {
-        // properties
+        // properties of Message
         public string SubscriberID { get; set; }
         public string Topic { get; set; }
         public string Payload { get; set; }
 
-        // func NewMessage
         public Message(string SubscriberID, string Topic, string Payload)
         {
             SubscriberID = subscriberID;
             Topic = topic;
             Payload = payload;
         }
+
+        // Allows to create a new message
         public static Message NewMessage(string subscriberID, string topic, string payload)
         {
             return new Message(subscriberID, topic, payload);
         }
-        //!todo tostring
+
+        // Returns the message as a string in the format "SubscriberID.Topic.Payload\n".
+        public override string ToString()
+        {
+            return $"{SubscriberID}.{Topic}.{Payload}\n";
+        }
     }
     public class HubClient
     {
-        // properties
+        // properties of HubClient
         public string subscriberID { get; set; }
         public string Topics { get; set; }
         public Action<Message> handler { get; set; } // func(Message)
         public IPEndPoint Address { get; set; } // net.TCPAddr
         public SslStream Conn { get; set; } // tls.Conn
         public bool Debug { get; set; }
+
+        // init HubClient with address & port
+        public HubClient(string address, int port)
+        {
+            Address == new IPEndPoint(IPAddress.Parse(address), port);
+            Debug = Environment.GetEnvironmentVariable("DEBUG") == "true";
+        }
 
     }
     class Programs
